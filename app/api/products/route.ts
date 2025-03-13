9f3bafc499b05d0e4d9e82996e3660ff2b3cd731
Abimael <abimael.spdev@gmail.com>
2025-03-13 18:54:45 -0400

    feat: add initial products dialog

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -4,7 +4,7 @@ import { db } from "@/app/_lib/prisma";
 
 export async function GET() {
   const products = await db.product.findMany({});
-  return Response.json(products, { status: 200 });
+  return Response.json({ products }, { status: 200 });
 }
 
 export async function POST(request: Request) {
---
+++ app/products/_components/add-product-button.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/_components/ui/dialog";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PlusIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/app/_components/ui/form";
+import { Input } from "@/app/_components/ui/input";
+import { NumericFormat } from "react-number-format";
+import { z } from "zod";
+import { DialogClose } from "@radix-ui/react-dialog";
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, { message: "Nome é obrigatório." }),
+  price: z.number().min(0.01, { message: "Preço é obrigatório." }),
+  stock: z.coerce
+    .number()
+    .positive({
+      message: "Estoque não pode ser negativo.",
+    })
+    .int()
+    .min(0, { message: "Estoque é obrigatório." }),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+const AddProductButton = () => {
+  const form = useForm<FormSchema>({
+    shouldUnregister: true,
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      price: 0,
+      stock: 1,
+    },
+  });
+
+  const onSubmit = (data: FormSchema) => {
+    console.log(data);
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="gap-2">
+          <PlusIcon size={20} />
+          Novo Produto
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <DialogHeader>
+              <DialogTitle>Criar Produto</DialogTitle>
+              <DialogDescription>
+                Insira as informações abaixo
+              </DialogDescription>
+            </DialogHeader>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite o nome do produto" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Preço</FormLabel>
+                  <FormControl>
+                    <NumericFormat
+                      thousandSeparator="."
+                      decimalSeparator=","
+                      fixedDecimalScale
+                      prefix="R$ "
+                      allowNegative={false}
+                      customInput={Input}
+                      onValueChange={(values) => {
+                        field.onChange(values.floatValue);
+                      }}
+                      {...field}
+                      onChange={() => {}}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="stock"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Estoque</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      placeholder="Digite o estoque do produto"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="reset" variant={"secondary"}>
+                  Cancelar
+                </Button>
+              </DialogClose>
+              <Button type="submit">Salvar</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddProductButton;
