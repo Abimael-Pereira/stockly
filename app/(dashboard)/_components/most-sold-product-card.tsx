533aa27b2ae964265aabe84853a37c32741e4249
Abimael <abimael.spdev@gmail.com>
2025-03-24 23:00:02 -0400

    feat: add suspense to most sold product component with streaming

---
+++ app/(dashboard)/_components/most-sold-product-card.tsx
@@ -0,0 +1,51 @@
+import { getMostSoldProducts } from "@/app/_data-access/dashboard/get-most-sold-products";
+import MostSoldProductsItem from "./most-sold-product-item";
+import { Skeleton } from "@/app/_components/ui/skeleton";
+
+const MostSoldProducts = async () => {
+  const { mostSoldProducts } = await getMostSoldProducts();
+
+  return (
+    <div className="flex h-full flex-col overflow-hidden rounded-xl bg-white">
+      <p className="p-6 text-lg font-semibold text-slate-900">
+        Produtos mais vendidos
+      </p>
+      <div className="space-y-7 overflow-y-auto px-6 pb-6">
+        {mostSoldProducts.map((product) => (
+          <MostSoldProductsItem key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const MostSoldProductItemSkeleton = () => {
+  return (
+    <div className="flex items-center justify-between space-y-2 pt-5">
+      <div className="space-y-2">
+        <div className="h-[22px] w-[91.23px] rounded-md bg-gray-200" />
+        <div className="h-5 w-[91.23px] rounded-md bg-gray-200" />
+        <div className="h-6 w-[105.23px] rounded-md bg-gray-200" />
+      </div>
+      <div>
+        <div className="h-5 w-[86.26px] rounded-md bg-gray-200" />
+      </div>
+    </div>
+  );
+};
+
+export const MostSoldProductsSkeleton = () => {
+  return (
+    <Skeleton className="bg-white p-6">
+      <div className="space-y-2">
+        <div className="h-5 w-52 rounded-md bg-gray-200" />
+        <MostSoldProductItemSkeleton />
+        <MostSoldProductItemSkeleton />
+        <MostSoldProductItemSkeleton />
+        <MostSoldProductItemSkeleton />
+      </div>
+    </Skeleton>
+  );
+};
+
+export default MostSoldProducts;
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -4,20 +4,20 @@ import Header, {
   HeaderTitle,
 } from "../_components/header";
 
-import MostSoldProductsItem from "./_components/most-sold-product-item";
 import TotalRevenueCard from "./_components/total-revenue-card";
 import TodayRevenueCard from "./_components/today-revenue-card";
 import TotalSalesCard from "./_components/total-sales-card";
 import TotalProductStock from "./_components/product-stock-card";
 import TotalProducts from "./_components/total-products";
-import { getMostSoldProducts } from "../_data-access/dashboard/get-most-sold-products";
 import { Suspense } from "react";
 import { SummaryCardSkeleton } from "./_components/summary-card";
 import Last14DaysRevenueCard from "./_components/last-14-days-revenue-card";
 import { Skeleton } from "../_components/ui/skeleton";
+import MostSoldProducts, {
+  MostSoldProductsSkeleton,
+} from "./_components/most-sold-product-card";
 
 const HomePage = async () => {
-  const { mostSoldProducts } = await getMostSoldProducts();
   return (
     <div className="m-8 flex w-full flex-col space-y-8 rounded-lg">
       <Header>
@@ -46,8 +46,8 @@ const HomePage = async () => {
           fallback={
             <Skeleton className="bg-white p-6">
               <div className="space-y-2">
-                <Skeleton className="h-5 w-[86.26px] rounded-md bg-gray-200" />
-                <Skeleton className="h-4 w-48 rounded-md bg-gray-200" />
+                <div className="h-5 w-[86.26px] rounded-md bg-gray-200" />
+                <div className="h-4 w-48 rounded-md bg-gray-200" />
               </div>
             </Skeleton>
           }
@@ -55,16 +55,9 @@ const HomePage = async () => {
           <Last14DaysRevenueCard />
         </Suspense>
 
-        <div className="flex h-full flex-col overflow-hidden rounded-xl bg-white">
-          <p className="p-6 text-lg font-semibold text-slate-900">
-            Produtos mais vendidos
-          </p>
-          <div className="space-y-7 overflow-y-auto px-6 pb-6">
-            {mostSoldProducts.map((product) => (
-              <MostSoldProductsItem key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
+        <Suspense fallback={<MostSoldProductsSkeleton />}>
+          <MostSoldProducts />
+        </Suspense>
       </div>
     </div>
   );
